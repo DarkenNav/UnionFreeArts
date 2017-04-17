ab3d766f15b87fc99618cadf79a6d253d90ca1b7
Navarro <altnavarro@gmail.com>
2017-04-17 18:14:08 +0300

    Добавлены DTO

---
+++ DesktopApp/UI.Desktop.UFart/DTO/Base/EntityBaseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Desktop.UFart.UI.DTO.Base
+{
+    public class EntityBaseDTO
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ DesktopApp/UI.Desktop.UFart/DTO/PageDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Desktop.UFart.UI.DTO.Base;
+
+namespace UI.Desktop.UFart.UI.DTO
+{
+    public class PageDTO
+        : EntityBaseDTO
+    {
+        public string Url { get; set; }
+        public int SiteID { get; set; }
+        public DateTime FoundDateTime { get; set; }
+        public DateTime LastScanDate { get; set; }
+    }
+}

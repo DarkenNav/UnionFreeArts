ce574f000aac37e63aafd6f6cff1c98b344adbe0
rkorupaev <rkorupaev@gmail.com>
2017-04-17 19:38:04 +0300

    рефактор коментариев

--- Croaker/crawler/src/main/java/ru/unionfreearts/crawler/crawler/RobotsParser.java
+++ Croaker/crawler/src/main/java/ru/unionfreearts/crawler/crawler/RobotsParser.java
@@ -7,25 +7,19 @@ import java.util.ArrayList;
 
 public class RobotsParser {
 
-	public ArrayList<String> robotParser() throws Exception {// на вход
-																// принимаем
-																// String URL,
-																// допишем
-																// потом, пока
-																// для теста.
-
-		URL url = new URL("http://yandex.ru/robots.txt");// заходим на сайт и
-															// читаем файл
-															// robots.txt
+	// На вход принимаем String URL, допишем потом, пока для теста.
+	public ArrayList<String> robotParser() throws Exception {
+		// Заходим на сайт и читаем файл robots.txt
+		URL url = new URL("http://yandex.ru/robots.txt");
 		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
 		String inputLine;
 		ArrayList<String> sitemapURL = new ArrayList<String>();
 
 		while ((inputLine = in.readLine()) != null) {
-			if (inputLine.contains("sitemap.xml")) { // ищем сслыку на
-														// sitemap.xml
-				//System.out.println(inputLine);// тут пока печатаем, в
+			// Ищем сслыки на sitemap.xml
+			if (inputLine.contains("sitemap.xml")) {
+				System.out.println(inputLine);// тут пока печатаем, в
 												// будущем
 												// делаем, что хотим.
 
@@ -36,7 +30,8 @@ public class RobotsParser {
 
 		}
 		in.close();
-		//System.out.println(sitemapURL);//печатать в финале не обязательно, здесь для проверки работоспособности
+		System.out.println(sitemapURL);// печатать в финале не обязательно,
+										// здесь для проверки работоспособности
 		return sitemapURL;
 
 	}

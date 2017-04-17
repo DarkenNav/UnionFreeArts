34ccf94d1f8f3a18b0f0d9be7d42eafee8124b38
rkorupaev <rkorupaev@gmail.com>
2017-04-17 21:18:42 +0300

    небольшой рефакторинг кода

--- Croaker/crawler/src/main/java/ru/unionfreearts/crawler/SitemapParser.java
+++ Croaker/crawler/src/main/java/ru/unionfreearts/crawler/SitemapParser.java
@@ -11,29 +11,28 @@ public class SitemapParser {
 		ArrayList<String> urlFromRobotParser = new ArrayList<String>(robotParser.robotParser());
 		ArrayList<String> htmlURL = new ArrayList<String>();
 		// цикл потом включим, чтобы парсить все ссылки из robots.txt
-		//for (int i = 0; i < urlFromRobotParser.size(); i++) {
+		// for (int i = 0; i < urlFromRobotParser.size(); i++) {
 
-			URL url = new URL(urlFromRobotParser.get(1)); // передаем нужный URL
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+		URL url = new URL(urlFromRobotParser.get(1)); // передаем нужный URL
+		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
-			String inputLine;
+		String inputLine;
 
-			while ((inputLine = in.readLine()) != null) { // парсим HTML
-				if (inputLine.contains("https")) {
-					System.out.println(inputLine);
-					// пишем их в лист, убираем ишние символы с начала и конца
-					// строк.
-					htmlURL.add(inputLine.substring(5, inputLine.length() - 6));
-				}
-
+		while ((inputLine = in.readLine()) != null) { // парсим HTML
+			if (inputLine.contains("https")) {
+				System.out.println(inputLine);
+				// пишем их в лист, убираем ишние символы с начала и конца
+				// строк.
+				htmlURL.add(inputLine.substring(5, inputLine.length() - 6));
 			}
-			in.close();
 
-			System.out.println(htmlURL);// для тестирования работы, потом можно
-										// удалить.
-			
+		}
+		in.close();
 
-		//}
+		System.out.println(htmlURL);// для тестирования работы, потом можно
+									// удалить.
+
+		// }
 		return htmlURL;
 
 	}

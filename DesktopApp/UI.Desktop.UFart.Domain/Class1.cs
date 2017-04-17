a9eccddfc1643649411e28526dda9db86134dc05
Navarro <altnavarro@gmail.com>
2017-04-17 12:31:30 +0300

    Создание проекта DesktopApp

---
+++ DesktopApp/UI.Desktop.UFart.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFart.Desktop.Domain
+{
+    public class Class1
+    {
+    }
+}

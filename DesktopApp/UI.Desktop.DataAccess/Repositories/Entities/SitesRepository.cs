b527c0b5ca3198050a7269c54b49151c141d6529
Navarro <altnavarro@gmail.com>
2017-04-18 19:08:44 +0300

    Прототип реализации интерфейса заменен на обобщенную реализацию

--- DesktopApp/UI.Desktop.DataAccess/Repositories/Entities/SitesRepository.cs
+++ DesktopApp/UI.Desktop.DataAccess/Repositories/Entities/SitesRepository.cs
@@ -11,63 +11,8 @@ using UFart.Desktop.Domain.Entity;
 namespace UFart.Desktop.DataAccess.Repositories.Entities
 {
     public class SitesRepository
-        : AbstractRepositoryBase<Site>, IEntityRepositoryBase<Site>
+        : EntityRepositoryBase<Site>
     {
-        public void Create(Site item) 
-        {
-            if (fakeData != null)
-            {
-                if (fakeData.FirstOrDefault(f => f.ID == item.ID) == null)
-                {
-                    fakeData.Add(item);
-                    return;
-                }
-            }
-
-            throw new NotImplementedException();
-        }
-
-        public Site Get(int id)
-        {
-            if (fakeData != null)
-            {
-                return fakeData.FirstOrDefault(f => f.ID == id);
-            }
-
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Site> GetAll()
-        {
-            if (fakeData != null)
-            {
-                return fakeData;
-            }
-            throw new NotImplementedException();
-        }
-
-        public void Remove(Site item)
-        {
-            if (fakeData != null)
-            {
-                fakeData.Remove(item);
-                return;
-            }
-            throw new NotImplementedException();
-        }
-
-        public void Update(Site item)
-        {
-            if (fakeData != null)
-            {
-                var toUpdateItem = Get(item.ID);
-                if (toUpdateItem != null)
-                {
-                    Mapper.Map(item, toUpdateItem);
-                }
-                return;
-            }
-            throw new NotImplementedException();
-        }
+        
     }
 }

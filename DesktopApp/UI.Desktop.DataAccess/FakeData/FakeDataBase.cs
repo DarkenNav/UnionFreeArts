2e506c860ab4a023439d6acd17254996b965f104
Navarro <altnavarro@gmail.com>
2017-04-18 19:03:56 +0300

    исправлен namespace

--- DesktopApp/UI.Desktop.DataAccess/FakeData/FakeDataBase.cs
+++ DesktopApp/UI.Desktop.DataAccess/FakeData/FakeDataBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UFart.Desktop.UI.FakeData
+namespace UFart.Desktop.DataAccess.FakeData
 {
     public class FakeDataBase
     {

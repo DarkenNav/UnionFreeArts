6e9a2bfaf478cbdc5785ea88c9d4ff1db9ce974f
NeoSvet <neosvet333@gmail.com>
2017-04-19 20:01:12 +0300

    созданы фейковые загрузчики

---
+++ MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/loaders/PersonsLoaderFake.java
@@ -0,0 +1,21 @@
+package ru.unionfreeart.ufart.loaders;
+
+import android.content.Context;
+
+import ru.unionfreeart.ufart.interfaces.ILoader;
+import ru.unionfreeart.ufart.repositories.ListRepositories;
+
+/**
+ * Created by NeoSvet on 19.04.2017.
+ */
+
+public class PersonsLoaderFake implements ILoader {
+    public void run(Context context) {
+        String[] mPersons = new String[]{"Putin", "Medvedev"};
+        ListRepositories list = new ListRepositories(context, ListRepositories.LIST_PERSONS);
+        for (int i = 0; i < mPersons.length; i++) {
+            list.add(mPersons[i]);
+        }
+        list.saveList();
+    }
+}
---
+++ MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/loaders/SitesLoaderFake.java
@@ -0,0 +1,21 @@
+package ru.unionfreeart.ufart.loaders;
+
+import android.content.Context;
+
+import ru.unionfreeart.ufart.interfaces.ILoader;
+import ru.unionfreeart.ufart.repositories.ListRepositories;
+
+/**
+ * Created by NeoSvet on 19.04.2017.
+ */
+
+public class SitesLoaderFake implements ILoader {
+    public void run(Context context) {
+        String[] mSites = new String[]{"lenta.ru", "site.ru"};
+        ListRepositories list = new ListRepositories(context, ListRepositories.LIST_SITES);
+        for (int i = 0; i < mSites.length; i++) {
+            list.add(mSites[i]);
+        }
+        list.saveList();
+    }
+}

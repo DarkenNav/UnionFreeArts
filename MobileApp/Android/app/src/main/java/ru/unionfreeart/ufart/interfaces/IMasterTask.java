d04b6dd296f8c195fb203149af3873a37f814d31
NeoSvet <neosvet333@gmail.com>
2017-04-20 22:18:58 +0300

    добавлен метод getContext

--- MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/interfaces/IMasterTask.java
+++ MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/interfaces/IMasterTask.java
@@ -1,9 +1,12 @@
 package ru.unionfreeart.ufart.interfaces;
 
+import android.content.Context;
+
 /**
  * Created by NeoSvet on 19.04.2017.
  */
 
 public interface IMasterTask {
+    Context getContext();
     void putResult(String msg);
 }

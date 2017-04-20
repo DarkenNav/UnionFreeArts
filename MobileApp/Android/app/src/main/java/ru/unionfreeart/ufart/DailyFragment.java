94e3c9494564df5630788b965716309c6c62013a
NeoSvet <neosvet333@gmail.com>
2017-04-20 22:24:41 +0300

    рефакторинг и усовершенствование

--- MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/DailyFragment.java
+++ MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/DailyFragment.java
@@ -2,11 +2,13 @@ package ru.unionfreeart.ufart;
 
 import android.app.DatePickerDialog;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -16,55 +18,124 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import ru.unionfreeart.ui.TableAdapter;
-import ru.unionfreeart.ui.TableRow;
+import ru.unionfreeart.ufart.entities.ListAdapter;
+import ru.unionfreeart.ufart.entities.TableAdapter;
+import ru.unionfreeart.ufart.entities.TableRow;
+import ru.unionfreeart.ufart.interfaces.ILoader;
+import ru.unionfreeart.ufart.interfaces.IMasterTask;
+import ru.unionfreeart.ufart.loaders.DailyLoaderFake;
+import ru.unionfreeart.ufart.loaders.PersonsLoaderFake;
+import ru.unionfreeart.ufart.loaders.SitesLoaderFake;
+import ru.unionfreeart.ufart.repositories.ListRepositories;
+import ru.unionfreeart.ufart.repositories.TableRepositories;
 
-public class DailyFragment extends Fragment implements View.OnClickListener {
+public class DailyFragment extends Fragment implements View.OnClickListener, IMasterTask {
+    private final String START = "start", FINISH = "finish", LOADER = "loader";
     private final int MIN_DAY = 1, MIN_MONTH = 3, MIN_YEAR = 2017;
     private final DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
     private DatePickerDialog.OnDateSetListener dateStartListener, dateFinishListener;
-    private MainActivity act;
+    private MainActivity activity;
+    private LoaderTask loader;
+    private Spinner spSite, spPerson;
+    private ListAdapter adSites, adPerson;
     private TableAdapter adTable;
     private EditText etStartDate, etFinishDate;
     private Date dateStart, dateFinish;
     private View container;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        act = (MainActivity) getActivity();
-        act.setTitle(getResources().getString(R.string.daily_statistics));
+        activity = (MainActivity) getActivity();
+        activity.setTitle(getResources().getString(R.string.daily_statistics));
         this.container = inflater.inflate(R.layout.fragment_daily, container, false);
         initSites();
         initPersons();
-        initDates();
         initTable();
+        initButton();
+        initPreferences();
+        initDates();
+        restoreActivityState(savedInstanceState);
         return this.container;
     }
 
+    private void restoreActivityState(Bundle state) {
+        if (state == null) { //first open fragment
+            TableRepositories table = new TableRepositories(activity, TableRepositories.TABLE_DAILY);
+            table.clearTable();
+            loader = new LoaderTask(DailyFragment.this);
+            ILoader loaderSites = new SitesLoaderFake();
+            ILoader loaderPersons = new PersonsLoaderFake();
+            loader.execute(loaderSites, loaderPersons);
+        } else { //restory fragment
+            loader = (LoaderTask) state.getSerializable(LOADER);
+            if (loader == null || loader.getStatus() != AsyncTask.Status.RUNNING) {
+                loader.newMaster(DailyFragment.this);
+            } else {
+                openLists();
+                openTable();
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable(LOADER, loader);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        editor.putLong(START, dateStart.getTime());
+        editor.putLong(FINISH, dateFinish.getTime());
+        editor.apply();
+    }
+
+    private void initPreferences() {
+        pref = activity.getSharedPreferences(this.getClass().getSimpleName(), activity.MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    private void initButton() {
+        container.findViewById(R.id.bApply).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (loader == null || loader.getStatus() != AsyncTask.Status.RUNNING) {
+                    loader = new LoaderTask(DailyFragment.this);
+                    ILoader loaderDaily = new DailyLoaderFake(
+                            adSites.getId(spSite.getSelectedItemPosition()),
+                            adPerson.getId(spPerson.getSelectedItemPosition()),
+                            dateStart, dateFinish);
+                    loader.execute(loaderDaily);
+                }
+            }
+        });
+    }
+
     private void initSites() {
-        Spinner spSite = (Spinner) container.findViewById(R.id.spSite);
-        ArrayAdapter<String> adSites = new ArrayAdapter<String>(act, android.R.layout.simple_spinner_dropdown_item);
-        adSites.add("lenta.ru");
-        adSites.add("site.ru");
+        spSite = (Spinner) container.findViewById(R.id.spSite);
+        adSites = new ListAdapter(activity);
         spSite.setAdapter(adSites);
     }
 
     private void initPersons() {
-        Spinner spPerson = (Spinner) container.findViewById(R.id.spPerson);
-        ArrayAdapter<String> adPerson = new ArrayAdapter<String>(act, android.R.layout.simple_spinner_dropdown_item);
-        adPerson.add("Путин");
-        adPerson.add("Медведев");
+        spPerson = (Spinner) container.findViewById(R.id.spPerson);
+        adPerson = new ListAdapter(activity);
         spPerson.setAdapter(adPerson);
     }
 
     private void initDates() {
-        dateStart = new Date();
+        long now = System.currentTimeMillis();
+        dateStart = new Date(pref.getLong(START, now));
+        dateFinish = new Date(pref.getLong(FINISH, now));
+
         etStartDate = (EditText) container.findViewById(R.id.etStartDate);
         etStartDate.setText(dateFormat.format(dateStart));
         etStartDate.setOnClickListener(this);
 
-        dateFinish = new Date();
         etFinishDate = (EditText) container.findViewById(R.id.etFinishDate);
         etFinishDate.setText(dateFormat.format(dateFinish));
         etFinishDate.setOnClickListener(this);
@@ -87,20 +158,7 @@ public class DailyFragment extends Fragment implements View.OnClickListener {
 
     private void initTable() {
         ListView lvTable = (ListView) container.findViewById(R.id.lvTable);
-        adTable = new TableAdapter(act);
-        adTable.addItem(new TableRow(getResources().getString(R.string.date),
-                getResources().getString(R.string.count_new_pages)));
-        adTable.getItem(0).setBold(true);
-        String[] mDates = new String[]{"15.02.2015", "16.02.2015", "17.02.2015",
-                "18.02.2015", "18.02.2015", "19.02.2015", "20.02.2015"};
-        int[] mCount = new int[]{1, 2, 0, 4, 1, 9, 2};
-        int k = 0;
-        for (int i = 0; i < mDates.length; i++) {
-            adTable.addItem(new TableRow(mDates[i], mCount[i]));
-            k += mCount[i];
-        }
-        adTable.addItem(new TableRow(getResources().getString(R.string.total_for_period), k));
-        adTable.getItem(adTable.getCount() - 1).setBold(true);
+        adTable = new TableAdapter(activity);
         lvTable.setAdapter(adTable);
     }
 
@@ -109,10 +167,10 @@ public class DailyFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         DatePickerDialog datePickerDialog;
         if (view.getId() == R.id.etStartDate) {
-            datePickerDialog = new DatePickerDialog(act, dateStartListener,
+            datePickerDialog = new DatePickerDialog(activity, dateStartListener,
                     dateStart.getYear(), dateStart.getMonth(), dateStart.getDay());
         } else { //if (view.getId() == R.id.etFinishDate)
-            datePickerDialog = new DatePickerDialog(act, dateFinishListener,
+            datePickerDialog = new DatePickerDialog(activity, dateFinishListener,
                     dateFinish.getYear(), dateFinish.getMonth(), dateFinish.getDay());
         }
         Date max = new Date();
@@ -126,5 +184,49 @@ public class DailyFragment extends Fragment implements View.OnClickListener {
         return year - 1900;
     }
 
+    public Context getContext() {
+        return activity;
+    }
 
+    public void putResult(String msg) {
+
+        openLists();
+        openTable();
+    }
+
+    private void openTable() {
+        adTable.clear();
+        TableRepositories table = new TableRepositories(activity, TableRepositories.TABLE_DAILY);
+        table.loadTable();
+        if (table.getCount() == 0) //table is empty
+            return;
+        adTable.addItem(new TableRow(getResources().getString(R.string.date),
+                getResources().getString(R.string.count_new_pages)));
+        adTable.getItem(0).setBold(true);
+        int k = 0;
+        for (int i = 0; i < table.getCount(); i++) {
+            adTable.addItem(new TableRow(table.getName(i), table.getValue(i)));
+            k += Integer.parseInt(table.getValue(i));
+        }
+        adTable.addItem(new TableRow(getResources().getString(R.string.total_for_period), k));
+        adTable.getItem(adTable.getCount() - 1).setBold(true);
+        adTable.notifyDataSetChanged();
+    }
+
+    private void openLists() {
+        if (adSites.getCount() > 0) //lists is not empty
+            return;
+        ListRepositories sites = new ListRepositories(activity, ListRepositories.LIST_SITES);
+        sites.loadList();
+        for (int i = 0; i < sites.getCount(); i++) {
+            adSites.addItem(sites.getItem(i));
+        }
+        adSites.notifyDataSetChanged();
+        ListRepositories persons = new ListRepositories(activity, ListRepositories.LIST_PERSONS);
+        persons.loadList();
+        for (int i = 0; i < persons.getCount(); i++) {
+            adPerson.addItem(persons.getItem(i));
+        }
+        adPerson.notifyDataSetChanged();
+    }
 }

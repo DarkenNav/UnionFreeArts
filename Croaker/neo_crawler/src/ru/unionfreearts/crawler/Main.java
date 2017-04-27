45b38f7487dbd10dbda265e3ca739fa5cbdb8415
NeoSvet <neosvet333@gmail.com>
2017-04-27 23:05:23 +0300

    добавлена возможность выбрать сайт для парсинга и команда для выхода

--- Croaker/neo_crawler/src/ru/unionfreearts/crawler/Main.java
+++ Croaker/neo_crawler/src/ru/unionfreearts/crawler/Main.java
@@ -1,11 +1,22 @@
 package ru.unionfreearts.crawler;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		try {
-			Parser parser = new Parser(1);
-			parser.start();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter id site for parsing:");
+			int id = sc.nextInt();
+			System.out.println("Enter q for quit");	
+			Parser parser = new Parser(id);
+			parser.start();			
+			String s = sc.nextLine();
+			while(!s.equals("q") && parser.isRun())
+				s = sc.nextLine();	
+			parser.stop();
 		} catch (Exception e) {
+			e.printStackTrace();
 			System.out.println("ERROR (Main): " + e.getMessage());
 		}
 	}

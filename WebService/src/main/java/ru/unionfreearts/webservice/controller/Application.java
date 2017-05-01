755e0f83294cd1f0b70dc946d85f150e4c72fc2e
Dmitry <Dmitry.k888@gmail.com>
2017-05-02 00:25:26 +0300

    refactor - добавлен пакет для сканирования компонентов

--- WebService/src/main/java/ru/unionfreearts/webservice/controller/Application.java
+++ WebService/src/main/java/ru/unionfreearts/webservice/controller/Application.java
@@ -2,8 +2,10 @@ package ru.unionfreearts.webservice.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.unionfreearts.webservice"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

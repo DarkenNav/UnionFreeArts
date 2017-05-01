4d152205fd5b5e1cb5a77f88d72fcecf178d826b
Dmitry <Dmitry.k888@gmail.com>
2017-05-02 00:17:26 +0300

    add - Добавлена абстрактная спецификация, реализующаа интерфейс Specification

--- WebService/src/main/java/ru/unionfreearts/webservice/dao/specifications/AbstractSpecification.java
+++ WebService/src/main/java/ru/unionfreearts/webservice/dao/specifications/AbstractSpecification.java
@@ -1,4 +1,21 @@
 package ru.unionfreearts.webservice.dao.specifications;
 
-public class AbstractSpecification {
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.lang.reflect.ParameterizedType;
+
+public class AbstractSpecification<T> implements Specification<T> {
+    @Override
+    public Predicate toPredicate(Root<T> root, CriteriaBuilder cb) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public Class<T> getType() {
+        ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
+        return (Class<T>) type.getActualTypeArguments()[0];
+    }
 }

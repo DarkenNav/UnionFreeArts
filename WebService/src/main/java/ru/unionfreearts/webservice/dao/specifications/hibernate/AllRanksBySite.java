0fd3ca8d9c211d603ccf54a061e61cdabba0698b
Dmitry <Dmitry.k888@gmail.com>
2017-05-02 00:27:30 +0300

    refactor - изменен метод получения общей статистики

--- WebService/src/main/java/ru/unionfreearts/webservice/dao/specifications/hibernate/AllRanksBySite.java
+++ WebService/src/main/java/ru/unionfreearts/webservice/dao/specifications/hibernate/AllRanksBySite.java
@@ -1,16 +1,25 @@
 package ru.unionfreearts.webservice.dao.specifications.hibernate;
 
 import ru.unionfreearts.webservice.dao.specifications.AbstractSpecification;
-import ru.unionfreearts.webservice.model.Person;
-import ru.unionfreearts.webservice.model.Rank;
+import ru.unionfreearts.webservice.model.*;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-public class AllRanks extends AbstractSpecification<Rank> {
+public class AllRanksBySite extends AbstractSpecification<Rank> {
+    private Long siteId;
+
+    public AllRanksBySite(Long siteId) {
+        this.siteId = siteId;
+    }
+
     @Override
     public Predicate toPredicate(Root<Rank> root, CriteriaBuilder cb) {
-        return cb.conjunction();
+        Path<Page> page = root.get(Rank_.page);
+        Path<Site> site = page.get(Page_.site);
+        Path<Long> id = site.get(Site_.id);
+        return cb.equal(id, siteId);
     }
 }

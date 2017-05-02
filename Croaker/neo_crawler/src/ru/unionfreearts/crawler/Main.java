08ec1dbab3a3c9f170f254e2e42caf0a5a1b69c7
NeoSvet <neosvet333@gmail.com>
2017-05-02 22:08:54 +0300

    реализация сканирования нескольких сайтов одновременно

--- Croaker/neo_crawler/src/ru/unionfreearts/crawler/Main.java
+++ Croaker/neo_crawler/src/ru/unionfreearts/crawler/Main.java
@@ -1,20 +1,30 @@
 package ru.unionfreearts.crawler;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
 			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter id site for parsing:");
-			int id = sc.nextInt();
-			System.out.println("Enter q for quit");	
-			Parser parser = new Parser(id);
-			parser.start();			
+			System.out.println("Enter q for quit");
+			ArrayList<Parser> parsers = new ArrayList<Parser>();
+			Parser p;
+			int i = 2;
+			while (true) {
+				p = new Parser(i);
+				if (p.start())
+					parsers.add(p);
+				else
+					break;
+				i++;
+			}
 			String s = sc.nextLine();
-			while(!s.equals("q") && parser.isRun())
-				s = sc.nextLine();	
-			parser.stop();
+			while (!s.equals("q"))
+				s = sc.nextLine();
+			for (i = 0; i < parsers.size(); i++) {
+				parsers.get(i).stop();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("ERROR (Main): " + e.getMessage());

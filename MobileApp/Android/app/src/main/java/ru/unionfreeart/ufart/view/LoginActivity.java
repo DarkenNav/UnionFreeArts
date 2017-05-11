879f49f83adbeaddf26a959d93de6eecf1d0f265
NeoSvet <neosvet333@gmail.com>
2017-05-11 20:18:10 +0300

    реализация интерфейса экрана авторизации

--- MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/view/LoginActivity.java
+++ MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/view/LoginActivity.java
@@ -1,29 +1,61 @@
 package ru.unionfreeart.ufart.view;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 import ru.unionfreeart.ufart.R;
+import ru.unionfreeart.ufart.utils.Settings;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText etLogin, etPassword;
+    private Settings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        initUI();
+        initButtons();
+    }
+
+    private void initButtons() {
+        findViewById(R.id.bUser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        findViewById(R.id.bAdmin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         findViewById(R.id.fabOk).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                settings.setLogin(etLogin.getText().toString());
+                settings.setLogin(etPassword.getText().toString());
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
+    }
+
+    private void initUI() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        settings = new Settings(LoginActivity.this);
+
+        etLogin = (EditText) findViewById(R.id.etLogin);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etLogin.setText(settings.getLogin());
+        etPassword.setText(settings.getPassword());
     }
 
 }

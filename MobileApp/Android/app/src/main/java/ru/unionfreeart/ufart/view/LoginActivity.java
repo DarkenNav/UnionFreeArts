e9bc2b4c97683eddcb655a2cf2e3b1d9bacf6c96
NeoSvet <neosvet333@gmail.com>
2017-05-11 19:51:56 +0300

    добавлен экран авторизации

---
+++ MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/view/LoginActivity.java
@@ -0,0 +1,29 @@
+package ru.unionfreeart.ufart.view;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ru.unionfreeart.ufart.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.fabOk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}

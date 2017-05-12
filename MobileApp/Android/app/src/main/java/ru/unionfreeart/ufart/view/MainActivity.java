5d6bf8af0971cd70d748476657cd4ba228bfe500
NeoSvet <neosvet333@gmail.com>
2017-05-12 10:54:23 +0300

    исправлено: восстановление MainActivity при разворачивании приложения

--- MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/view/MainActivity.java
+++ MobileApp/Android/app/src/main/java/ru/unionfreeart/ufart/view/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+                MainActivity.this.finish();
             }
         });
     }

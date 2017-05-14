a19be6d699b6e8e4637c5f9f68a30a64006ec119
Navarro <altnavarro@gmail.com>
2017-05-14 13:09:59 +0300

    1

---
+++ DesktopApp/UI.Desktop.UFart/Interface/FormStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop.UFart.Interface
+{
+    public partial class FormStart : Form
+    {
+        public FormStart()
+        {
+            InitializeComponent();
+        }
+    }
+}
